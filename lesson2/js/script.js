9ee0549e7b576deef56ed3e842913dfe84c01677
marcusBYUI <ojo18002@byui.edu>
2022-01-20 15:31:58 +0100

    changed footer to last modified

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,16 +1,7 @@
 const year = new Date().getFullYear();
 
 
-const dateObj = new Date();
+// to get last modiified
+const string = document.lastModified;
 
-//to get that dates
-const month = dateObj.getUTCMonth() + 1; //months from 1-12
-const day = dateObj.getUTCDate();
-
-// to get time
-const hours = dateObj.getHours();
-const minutes = dateObj.getMinutes();
-const seconds = dateObj.getSeconds();
-
-
-document.querySelector(".year").innerHTML = `© ${year} | Marcus A. Ojo-Osasere | Last Updated: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`
+document.querySelector(".year").innerHTML = `© ${year} | Marcus A. Ojo-Osasere | Last Updated: ${string}`
