5066405aa690b2ad70f88e654a7b978da6b90298
marcusBYUI <ojo18002@byui.edu>
2022-02-04 12:38:51 +0100

    book of mormon list

---
+++ book-of-mormon/script/book-of-mormon.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("#favList");
+const input = document.querySelector("#fav");
+const button = document.querySelector("#submitFav");
+
+button.onclick = function() {
+    let myItem = input.value;
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("p");
+    const listBtn = document.createElement("span");
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "❌";
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e) {
+        list.removeChild(listItem);
+    };
+
+    input.focus();
+};
