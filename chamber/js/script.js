cf89ba46061785e46e9900a10fe08096c4214b66
MarcusBYUI <ojo18002@byui.edu>
2022-03-18 13:41:00 +0100

    contact page completion

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,44 +1,39 @@
 function toggleMenu() {
-    document.querySelector(".primary").classList.toggle("open");
-    document.querySelector("#ham").classList.toggle("open");
+  document.querySelector(".primary").classList.toggle("open");
+  document.querySelector("#ham").classList.toggle("open");
 }
 
 const x = document.querySelector("#ham");
 x.onclick = toggleMenu;
 
-
-
-const fBase = document.querySelector(".fb");
-const fMed = document.querySelector(".fm");
-const fLarge = document.querySelector(".fl");
-
-
+const fyear = document.querySelectorAll(".fyear");
+const fMod = document.querySelectorAll(".fMod");
 
 //day names array
 const daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
 // month name array
 const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 const today = new Date();
@@ -49,46 +44,47 @@ const dateNum = today.getDate();
 const dayNum = today.getDay();
 const year = today.getFullYear();
 
+for (let yearTag = 0; yearTag < fyear.length; yearTag++) {
+  fyear[yearTag].innerHTML = year;
+  fMod[yearTag].innerHTML = document.lastModified;
+}
 
-fBase.innerHTML = `<strong>&copy; ${year} Uyo Chammber <br> Ojo-Osasere Marcus <br> WDD 230 Project <br> Last Modification: ${document.lastModified} </strong>`;
-fMed.innerHTML = `<strong>&copy; ${year} Uyo Chammber | Ojo-Osasere Marcus | WDD 230 Project |<br> Last Modification: ${document.lastModified} </strong>`;
-fLarge.innerHTML = `<strong>&copy; ${year} Uyo Chammber | Ojo-Osasere Marcus | WDD 230 Project | Last Modification: ${document.lastModified} </strong>`;
-
-document.querySelector(".date").innerHTML = `<p><strong>${dayName}, ${dateNum} ${monthName} ${year}</strong></p>`;
+document.querySelector(
+  ".date"
+).innerHTML = `<p><strong>${dayName}, ${dateNum} ${monthName} ${year}</strong></p>`;
 
 window.addEventListener("load", () => {
-    if (dayNum == 1 || dayNum == 2) {
-        document.querySelector(".banner").style.display = "block";
-    } else {
-        document.querySelector(".banner").style.display = "none";
-    }
+  if (dayNum == 1 || dayNum == 2) {
+    document.querySelector(".banner").style.display = "block";
+  } else {
+    document.querySelector(".banner").style.display = "none";
+  }
 });
-
 
 // set active
 
 function active() {
-    switch (document.title) {
-        case "Uyo Chamber of Commerce":
+  switch (document.title) {
+    case "Uyo Chamber of Commerce":
+      document.querySelector("#home").classList.add("active");
+      break;
 
-            document.querySelector("#home").classList.add("active");
-            break;
+    case "Uyo Chamber of Commerce - Discover":
+      document.querySelector("#discover").classList.add("active");
+      break;
 
-        case "Uyo Chamber of Commerce - Discover":
-            document.querySelector("#discover").classList.add("active");
-            break;
+    case "Uyo Chamber of Commerce - Join":
+      document.querySelector("#join").classList.add("active");
+      break;
 
-        case "Uyo Chamber of Commerce - Join":
-            document.querySelector("#join").classList.add("active");
-            break;
+    case "Uyo Chamber of Commerce - Directory":
+      document.querySelector("#directory").classList.add("active");
+      break;
 
-        case "Uyo Chamber of Commerce - Directory":
-            document.querySelector("#directory").classList.add("active");
-
-
-
-    }
-
+    case "Uyo Chamber of Commerce - Contact":
+      document.querySelector("#contact").classList.add("active");
+      break;
+  }
 }
 
 active();
